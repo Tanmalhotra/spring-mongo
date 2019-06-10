b87a27a111b507d2089e9d0f650ccecbf7371042
Tan <gzkm4839@orange.com>
2019-06-10 14:38:14 +0530

    commit with skills working

--- src/App.js
+++ src/App.js
@@ -1,40 +1,82 @@
 import React from 'react';
-//import Creatable from 'react-select';
-import { Creatable } from 'react-select';
 
-
-/*const inputParsers = {
- 
-  uppercase(input) {
-    return input.toUpperCase();
-  },
-  number(input) {
-    return parseFloat(input);
-  },
-};*/
-
+import Creatable from 'react-select/creatable';
 
 class App extends React.Component {
-	
 	
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
 			isLoading: true,
-		    selectedOption: null,
+		    selected: null,
 		    PIMS:'',
 	    	  name:'',
 	    	  teamName:'',
 	    	  email:'',
 	    	  CUID:'',
 	    	  skill:[],
-	    	  
+	    	  id:'',
+	    	  seq:0,
+	    	  	    	  
 		  };
    
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSelect=this.handleSelect.bind(this);
   }
+  
+  renderInput = props => {
+	    delete props.value;
+	    return (
+	      <div class='Select-input' style={{ display: 'inline-block' }}>
+	        <input {...props}/>
+	      </div>
+	    )
+	  }
+  
+  handleSelect=(selectedOption)=>{
+      console.log(`Option selected on creation is:`, selectedOption);
+      let select=this.state.selected;
+    
+      
+	    
+	  fetch('http://localhost:8087/api/skills/', {
+	      method: 'POST',
+	      headers:{
+	    	  
+	    	  "Content-Type":"application/json",
+	    	  "Acess-Contol-Allow-Origin":"*",
+	    	  "Accept":"application/json",
+	    	  "Access-Control-Allow-Methods":"*",
+	    	  "Access-Control-Allow-Headers":"*",
+	      },      
+	      mode:'cors', 
+	      
+	      body:JSON.stringify({
+	    	  id:Math.random()*1000,
+	    	  skill:selectedOption, 
+	      })
+	    })
+	    .then(response => response.json())
+	    .then(json => {
+	    
+	    console.log(json)
+
+	    select.push({value:json.id,label:json.skill});
+	    this.setState({selected:select}); 
+	    this.setState({data:select}); 
+	    console.log();
+	    }
+	    )
+	    .catch(err => console.log(err));
+
+	  console.log("printing state");
+	  console.log(this.state);
+	 console.log(selectedOption);
+	  
+  }
+  
   componentDidMount() {
 	    fetch(`http://localhost:8087/api/skills/`,{ method: 'GET',
 	      headers:{
@@ -60,39 +102,30 @@ class App extends React.Component {
 	        	  
 	         }
 	          this.setState({data:options})
+	          this.handleSelect=this.handleSelect.bind(this);
+	  	      this.handleChange = this.handleChange.bind(this);
+
 	    	  
 	      });
-	          
-	    
-	  }
+	           	  }
 
- 
-    
-    
   handleChange = (selectedOption) => {
-	    this.setState({selectedOption});  
-	    // this.setState= selectedOption.target.value;
+	    this.setState({selected:selectedOption});  
 	    console.log(`Option selected:`, selectedOption);
-	    
+        this.handleSelect=this.handleSelect.bind(this);
+
 	    this.handleChange = this.handleChange.bind(this);
 	    this.handleSubmit = this.handleSubmit.bind(this);
 	  }
-  
+
   handleSubmit(event) {
-	 /* console.log(event);*/
-	
+	this.handleChange = this.handleChange.bind(this);
+	this.handleSelect = this.handleSelect.bind(this);
     event.preventDefault();
     const form = event.target;
     const data = new FormData(form);
- /*   console.log(form);
-	console.log(event);*/
-	
-	 /* console.log(event.target[0].value);
-	  console.log(event.target[1].value);
-	  console.log(event.target[2].value);
-	  console.log(event.target[3].value);
-	  console.log(event.target[4].value);*/
-	  var select=this.state.selectedOption;
+
+	  var select=this.state.selected;
 	  console.log("selected value");
 	  console.log(select);
 	  let label;
@@ -103,14 +136,12 @@ class App extends React.Component {
 			  label = label + ", "+select[i]['label'];
 		  }
 	  }
-	 
 	    console.log(label);
 	    var labelTemp = label;
 	    let value = event.target.value;
-	    //this.setState({ type: value, label: label});
 	
-	  
-    fetch('http://localhost:8087/api/users/', {
+	  console.log(labelTemp);
+      fetch('http://localhost:8087/api/users/', {
       method: 'POST',
       headers:{
     	  
@@ -122,46 +153,44 @@ class App extends React.Component {
       },      
       mode:'cors', 
       
-      
       body:JSON.stringify({
     	  PIMS:event.target[0].value,
     	  name:event.target[1].value,
     	  teamName:event.target[2].value,
     	  email:event.target[3].value,
     	  CUID:event.target[4].value,
-    	  Skills: labelTemp,
+    	  skill: labelTemp,
       })
     })
+    
     .then(response => console.log(response))
     .catch(err => console.log(err));
 	console.log(data);
+	console.log(labelTemp);
+	
+console.log(label.split(","));
+	
   }
  
-
   render() {
-	const { selectedOption } = this.state;
+	const { selected } = this.state;
     return (
     		<div>
     		<center>
      <h1 className="head1"> WELCOME </h1>
      <h2 className="head2"> Kindly fill your details. </h2>
-      <form name="form" onSubmit={this.handleSubmit}>
-      
+      <form name="form" onSubmit={this.handleSubmit}> 
       <br />
-      
-    	 
+       	 
       <input
       id="PIMS"
       name="PIMS"
       type="text"
     	  placeholder="PIMS"
       data-parse="uppercase"
-      required
-      
-       
+      required          
     /><br /><br />
     
-
         <input
         id="name"
         	name="name"
@@ -171,9 +200,7 @@ class App extends React.Component {
           data-parse="uppercase"
          value={this.handleInputChange}
         /><br /><br />
-        
-    
-   
+          
       <input
       id="teamName"
     	  name="teamName"
@@ -181,19 +208,15 @@ class App extends React.Component {
     	  placeholder="Team Name"
       type="text"
       data-parse="uppercase"
-    	  required
-    	 
+    	  required  	 
     /><br /><br />
     
-     
-      
         <input ref="email" type="email"
         	name="name" id="name"
         		placeholder="E-mail"
         	/>
         	<br /><br />
-        	
-
+        
         <input
           ref="CUID"
         	  name="name" id="name"
@@ -201,23 +224,30 @@ class App extends React.Component {
         	  placeholder="CUID"
           data-parse="number"
         /><br /><br /><br />
-        
-        
+               
         <Creatable
         name="skill"
         id="skill"
         ref="skill"
         
         isMulti={true}
-        value={selectedOption}
-        
+        value={selected}
         onChange={this.handleChange}
+        onCreateOption={this.handleSelect}
         options={this.state.data}
+        onSelectResetsInput={false}
+        onBlurResetsInput={false}
+        onCloseResetsInput={false}
+        arrowRenderer={() => null}
+        clearRenderer={() => null}
+        allowCreate={true}
+        inputRenderer={this.renderInput} 
+        
+        
+        
         placeholder="Select Your skill"
       /> <br />
-        <br />
-      
-       
+        <br />     
 
         <button type="submit" className="myButton">Send data!</button>
         
@@ -227,53 +257,5 @@ class App extends React.Component {
     );
   }
 }
-
-/*async function copyOptionsForAsync() {
-	  let response = await fetch("https://jsonplaceholder.typicode.com/todos");
-	  let data = await response.json();
-	  data.forEach(element => {
-	    let dropDownEle = { label: element["skill"], value: element };
-	    options1.push(dropDownEle);
-	  });
-	}*/
-
-
-/*var options;
-fetch('http://localhost:8087/api/skill/', {
-	      method: 'GET',
-	      headers:{
-	    	  
-	    	  "Acess-Contol-Allow-Origin":"*",
-	    	  "Accept":"application/json",
-	    	  "Access-Control-Allow-Methods":"*",
-	    	  "Access-Control-Allow-Headers":"*",
-	      },      
-	      mode:'cors',
-
-	      }).then(response => response.text())
-	      .then(text => {
-	          var json = JSON.parse(text);
-	          console.log(json);
-	          options = [];
-	          for(var i=0;i<json.length;i++){
-	        	  console.log(json[i]);
-	        	  options.push({label:json[i].id,value:json[i].skill});
-	        	  console.log("options");
-	        	  console.log(options);
-	          }
-	          options = JSON.stringify(options);
-	          console.log(options)
-	          return text;
-	        })*/
-
-/*getskill(){
-	   return fetch(`http://localhost:8087/api/skill/?q=${this.state.firstSelectValue}`)
-	     .then((response) => response.json())
-	     .then((json) => {
-	        return { options: json.items };
-	    });
-	}
-*/
-
 
 export default App;
